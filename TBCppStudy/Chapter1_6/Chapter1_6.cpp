d51ae72aeae95adc2d3ef9d03b893d9e3e92d9d7
bcyoo <89197951+bcyoo@users.noreply.github.com>
2023-07-11 17:24:40 +0900

    키워드 식별자 정의

---
+++ TBCppStudy/Chapter1_6/Chapter1_6.cpp
@@ -0,0 +1,22 @@
+
+
+// 변수, 함수명 숫자로 시작하지 않음.
+
+void getNumber() {
+
+}
+
+class Myclass {
+public:
+	int m_variable;
+};
+
+int main() {
+
+	int value_a;
+	int nValue;
+	int n_Value;
+	int _value;
+
+	return 0;
+}
