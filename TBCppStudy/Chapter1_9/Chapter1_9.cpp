f53ca7650c9554e1f458cc268a6980f08e7c9637
bcyoo <89197951+bcyoo@users.noreply.github.com>
2023-07-12 15:17:34 +0900

    Formatting

---
+++ TBCppStudy/Chapter1_9/Chapter1_9.cpp
@@ -0,0 +1,20 @@
+/*
+Formatting
+*/
+
+//#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int add(int x, int y) {
+	return x + y; // tab키로 띄어쓰기 indenting
+}
+
+int main() {
+
+	cout << "Hello, World" << "" <<
+		 "" << endl;
+
+	return 0;
+}
