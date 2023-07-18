92ffdfefc514ef84e12d87606d0c2ffefde04e81
bcyoo <89197951+bcyoo@users.noreply.github.com>
2023-07-18 18:14:08 +0900

    산술연산자 1

---
+++ TBCppStudy/Chapter3_2/Chapter3_2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main() {
+
+	using namespace std;
+
+	//int x = 7;
+	//int y = 4; // - x일 경우 컴파일 문제 일어나지 않음
+	//int y1 = 1 - -x; // = 1 + x
+	//int z = x + y;
+
+	//int z1 = x % y;
+
+
+	//cout << x / y << " " << z1 << endl;
+
+	int x = 7;
+	int y = 4;
+
+	cout << x / y << endl;
+
+	cout << 
+	return 0;
+}
