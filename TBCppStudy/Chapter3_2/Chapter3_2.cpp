06fb1715f0afc526ecdfe56092615f3e44de8f7f
bcyoo <89197951+bcyoo@users.noreply.github.com>
2023-07-19 09:43:19 +0900

    산술 연산자 2

--- TBCppStudy/Chapter3_2/Chapter3_2.cpp
+++ TBCppStudy/Chapter3_2/Chapter3_2.cpp
@@ -19,6 +19,18 @@ int main() {
 
 	cout << x / y << endl;
 
-	cout << 
+
+	// 둘중에 하나만 실수일 경우 실수를 출력
+	cout << float(x) / y << endl;
+	cout << x / float(y) << endl;
+	cout << float(x) / float(y) << endl;
+
+	// 왼쪽 숫자(-5)가 음수면 결과는 음수로 출력
+	// 왼쪽 숫자(5)가 양수면 결과는 양수로 출력
+	cout << -5 % 2 << endl;
+
+	int z = x;
+	z += y; // z = z + y, *= /= %= += -=
+
 	return 0;
 }
