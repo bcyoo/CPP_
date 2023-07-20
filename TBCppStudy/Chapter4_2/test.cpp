1b4f90fc4f46f35588133f4fd50cdbc6e0b77f6c
bcyoo <89197951+bcyoo@users.noreply.github.com>
2023-07-20 18:08:03 +0900

    전역변수 정적변수 내부 연결 외부연결

---
+++ TBCppStudy/Chapter4_2/test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "MyConstants.h"
+
+extern int a = 1;
+
+void doSomething()
+{
+	using namespace std;
+	cout << "Hellow" << endl;
+
+	cout << "In test.cpp file " << Constants::pi << " " << &Constants::pi << endl;
+
+}
