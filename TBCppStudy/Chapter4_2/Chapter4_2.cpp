98b640989214ee8a74c17c4af07a1c96f11da67a
bcyoo <89197951+bcyoo@users.noreply.github.com>
2023-08-10 21:53:13 +0900

    전역변수, extern에 차이

--- TBCppStudy/Chapter4_2/Chapter4_2.cpp
+++ TBCppStudy/Chapter4_2/Chapter4_2.cpp
@@ -1,90 +1,45 @@
-// 전역 변수 Global Variable
-// 정적 변수 Static Variable
-// 내부 연결 Internal Linkage 
-// 외부 연결 External Linkage
-
-#include <iostream>
-#include "MyConstants.h"
+#include<iostream>
+#include"MyConstants.h"
 
 using namespace std;
-void doSomething();
+
+//static int g_a = 1; 
+//// static 을 붙이면 다른 cpp파일에서 전역변수를 사용할 수없게 하게됨
+//// 이유는 static을 사용했기에 이 cpp 파일 안에서 영역이 제한됨
+
+
+
+// forwart declaration 전방선언
+// main.cpp 파일을 컴파일 할때
+// main.cpp 파일을 컴파일 할때
+// 어딘가 cpp파일에 
+// extern void doSomething(); 존재하니까 빌드할 때 링킹해줌
+//extern void doSomething(); // extern이 생략됨
+//extern int a;
+
 
 /*
-	int g_x; // external linkage // 초기화가 안된 전역 변수 선언
-	static int g_x; // internal linkage // 다른 cpp파일로 부터 접근할 수 없음, 초기화가 되지 않는 정적 변수
-	const int g_xl // X 불가능 // const는 값을 안바꾸게 다는 조건이라 선언 불가능 > = 0 초기화를 해줘야함
+	int g_x; // external linkage, 초기화가 안된 전역변수 
+	static int g_x // interanl linkage 초기화가 안된 정적변수
+	const int g_x // 사용 불가 
 
-
-	forward declaration일 경우
-	extern을 붙여줌
+	forwart declaration
 	extern int g_z;
-	extern const int g_z; const를 붙일 수 있음, 다른 곳 어디선가에서만 값을 초기화 해줘야함
+	extern const int g_z; // 다른곳 한 곳에서 초기화가 되어있으면 const 사용가능
 
-	int g_y(1); extern 생략한 것임, 초기화가 된 전역변수를 뜻함
-	static int g_y(1); 외부에서 쓸 수 없는, 다른 cpp에서 접근이 불가능한 Internal Linkage인 전역변수
-	const int g_y(1); 일반적인 상수 선언, 같은 파일 안에서만 접근하는 상수
+	int g_y(1); external linkage, 초기화가 된 전역변수 
+	const int g_y(1); 같은 cpp에서만 접근 가능한 초기화 된 상수
+	static int g_y(1); interanl linkage, 다른 cpp에서 사용할 수 없는 초기화가 된 전역변수
 
-	extern int g_w(1); 초기화도하고, 전역변수이기도하고, 다른 cpp파일에서 접근가능, 다른곳에서 초기화를 하면 안됨
-	extern const in g_w(1);  상수고, 초기화됐고, 외부에서도 접근할 수 있음, 즉 다른 cpp에서 접근 가능
+	extern int g_w(1); external linkage, 초기화가된 전역변수, 다른 cpp에서 접근화 된 전역변수
+	extern const int g_w(1) 초기화가 된 상수, 다른 cpp에서 접근가능 한 상수
 */
 
 
+void doSomething();
 
-
-#pragma region global variable
-//int g_a = 1;
-#pragma endregion
-
-#pragma region static variable
-//static int s_a = 1; // 메모리가 정적임]
-//void doSomething(int a) {
-//	/*
-//	static int a = 1; 
-//	이 {} 영역안에 변수를 선언될 때 같은 메모리를 사용, 초기화를 한번 밖에 안함
-//	2~3번 실행될 때 값이 증가된 경우
-//	변수 a가 os로 부터 받은 메모리가 static이라는 뜻
-//	*/
-//	//static int a = 1; // 1로 초기화가 반드시 되어있어야함, 디버깅 시 좋음
-//	++g_a;
-//	cout << g_a << endl;
-//}
-#pragma endregion
-
-#pragma region External linkage
-// forward delaration
-//extern void doSomething(); // test.cpp에 있는 dosomething 함수가 존재하니까 초기화해줌, extern 키워드가 생략가능
-//extern int a;
-#pragma endregion
-
-int main() {
-
-#pragma region gloval variable
-	//cout << "global variable  " << value << endl;
-	
-	//// 지역 변수
-	//// {} 벗어나는 순간 메모리가 os로 반납
-	//// scope, duration 제한됨
-	//int value = 1;
-	//cout << "local variable  " << value << endl;
-	
-	//// global scope operatior
-	//cout << "global variable, :: " << ::value << endl;
-#pragma endregion
-
-#pragma region static variable
-	//int a = 10;
-	//doSomething(a); //2
-	//doSomething(a); //3
-	//doSomething(a); //4
-	//doSomething(a); //5
-#pragma endregion
-
-#pragma region external linkage
-	//doSomething();
-	//cout << a << endl;
-#pragma endregion
-
-
+int main() 
+{
 	cout << "In main.cpp file " << Constants::pi << " " << &Constants::pi << endl;
 	doSomething();
 
--- TBCppStudy/Chapter4_2/test.cpp
+++ TBCppStudy/Chapter4_2/test.cpp
@@ -6,7 +6,7 @@ extern int a = 1;
 void doSomething()
 {
 	using namespace std;
-	cout << "Hellow" << endl;
+	//cout << "Hellow" << endl;
 
 	cout << "In test.cpp file " << Constants::pi << " " << &Constants::pi << endl;
 
