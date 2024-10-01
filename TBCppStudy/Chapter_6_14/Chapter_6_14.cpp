ee6437c3801039b493b7e7c3ee3bfda896eafae9
bcyoo-dix <bcyoo91@gmail.com>
2024-10-01 18:24:01 +0900

    6-14

--- TBCppStudy/Chapter_6_14/Chapter_6_14.cpp
+++ TBCppStudy/Chapter_6_14/Chapter_6_14.cpp
@@ -2,14 +2,15 @@
 
 using namespace std;
 
-int main()
+
+void study_()
 {
 	int value = 5;
 
 	int* ptr = nullptr;
-	
+
 	ptr = &value;
-	int &ref = value;
+	int& ref = value;
 
 	//cout << ref << endl;
 
@@ -20,7 +21,34 @@ int main()
 	cout << &value << endl;
 	cout << &ref << endl;
 	cout << ptr << endl;
-	cout << &ptr << endl;// 포인터 자체 변수의 주소가 출력됨
+	cout << &ptr << endl; // 포인터 자체 변수의 주소가 출력됨
+}
+
+struct Something
+{
+		int v1;
+		float v2;
+};
+
+struct Other
+{
+	Something st;
+};
+int main()
+{
+	//Other ot;
+	//int &v1 = ot.st.v1;
+	//v1 = 1;
+
+	int value = 5;
+
+	// 두개 기능이 동일함
+	int *const ptr = &value;
+	int &ref = value;
+
+	*ptr = 10;
+	ref = 10;
+
 	return 0;
 }
 
